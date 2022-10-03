cb28e433758fb87d2c551e65810a2f3d03762f67
kleberandradejua <kleberandrade98@gmail.com>
2022-10-03 19:56:02 -0300

    Configuração Cypress

---
+++ web/cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
