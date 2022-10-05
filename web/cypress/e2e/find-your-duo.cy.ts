358c3359405124631b5f71408df2d5ebe5ab15bd
kleberandradejua <kleberandrade98@gmail.com>
2022-10-05 20:30:49 -0300

    Preenchendo e selecionando os campos do form

--- web/cypress/e2e/find-your-duo.cy.ts
+++ web/cypress/e2e/find-your-duo.cy.ts
@@ -2,22 +2,39 @@
 
 describe('find-your-duo', () => {
 
+  const selectorGame = 'button[data-cy="select-game"]'
+
   beforeEach(function(){
     cy.visit('http://127.0.0.1:5173/')
-    cy.get('.text-6xl').contains('Encontre seu')
-    
+    cy.get('.text-6xl').contains('Encontre seu')    
   })
 
   it('Publicar um anúncio', () => {
-
-    
-
     cy.get('.py-3')
     .contains('Publicar anúncio')
     .click()
     cy.get('h2[id="radix-:r4:"]').contains('Publique um anúncio')
-    cy.get('button[data-cy="select-game"]').click()
-    
-    
+    //cy.get(selectorGame).click()
+    cy.get('#name')
+    .click()
+    .type('Kleber de Andrade')
+    cy.get('#yearsPlaying')
+    .click()
+    .type('2')
+    cy.get('button[title="Domingo"]').click()
+    cy.get('button[title="Segunda"]').click()
+    cy.get('button[title="Terça"]').click()
+    cy.get('button[title="Quarta"]').click()
+    cy.get('button[title="Quinta"]').click()
+    cy.get('button[title="Sexta"]').click()
+    cy.get('button[title="Sábado"]').click()
+    cy.get('#hourStart')
+    .click()
+    .type('18:00')
+    cy.get('#hourEnd')
+    .click()
+    .type('22:00')
+    cy.get('button[role="checkbox"]').click()
+    cy.get('button[type="submit"]').click()
   })
 })
